53b2375dee93ae18c5200820f9c0a61109624e18
Mithraa J <mithraa2325@gmail.com>
2025-10-02 14:15:38 +0530

    Delete SecureVoteAI directory

--- SecureVoteAI/blueprints/results/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, request, redirect, url_for, flash, jsonify
-from . import results_bp
-
-@results_bp.route('/live')
-def live():
-    """Live results"""
-    return jsonify({'message': 'Live results endpoint'})
